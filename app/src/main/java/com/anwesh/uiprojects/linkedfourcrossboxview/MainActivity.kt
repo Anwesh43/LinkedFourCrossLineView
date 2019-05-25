06c2bea7cc746a1de465f890b80b7330b564a7a4
Anwesh43 <anweshthecool0@gmail.com>
2019-05-25 17:52:00 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedfourcrossboxview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedfourcrossboxview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedfourcrossboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.fourcrossboxview.FourCrossBoxView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FourCrossBoxView.create(this)
     }
 }
