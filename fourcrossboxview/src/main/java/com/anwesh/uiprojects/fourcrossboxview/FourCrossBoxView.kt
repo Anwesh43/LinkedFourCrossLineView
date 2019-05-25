58e00b9ed51f36fca5592f853202a51bf605b1a8
Anwesh43 <anweshthecool0@gmail.com>
2019-05-25 17:30:06 +0530

    adding canvas draw extension method

--- fourcrossboxview/src/main/java/com/anwesh/uiprojects/fourcrossboxview/FourCrossBoxView.kt
+++ fourcrossboxview/src/main/java/com/anwesh/uiprojects/fourcrossboxview/FourCrossBoxView.kt
@@ -11,6 +11,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 
 val nodes : Int = 5
 val lines : Int = 4
@@ -30,3 +31,36 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawCrossBox(i : Int, size : Float, sc : Float, paint : Paint) {
+    val sc1 : Float = sc.divideScale(0, 2)
+    val sc2 : Float = sc.divideScale(1, 2)
+    val a : Float = size / 2
+    val b : Float = a * sc1
+    val c : Float = 2 * size * sc2
+    save()
+    rotate(90f * i)
+    drawLine(a, a, a + b, a + b, paint)
+    drawLine(size, -size, size, -size + c, paint)
+    restore()
+}
+
+fun Canvas.drawCBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    for (j in 0..(lines - 1)) {
+        drawCrossBox(j, size, sc1.divideScale(j, lines), paint)
+    }
+    restore()
+}
