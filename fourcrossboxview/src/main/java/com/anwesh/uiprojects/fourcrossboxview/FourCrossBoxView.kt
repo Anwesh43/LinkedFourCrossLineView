bf8815dcbf0d4342e182f142ea09592ef7edb959
Anwesh43 <anweshthecool0@gmail.com>
2019-05-25 17:45:29 +0530

    adding FourCrossBox class

--- fourcrossboxview/src/main/java/com/anwesh/uiprojects/fourcrossboxview/FourCrossBoxView.kt
+++ fourcrossboxview/src/main/java/com/anwesh/uiprojects/fourcrossboxview/FourCrossBoxView.kt
@@ -173,4 +173,28 @@ class FourCrossBoxView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class FourCrossBox(var i : Int) {
+
+        private val root : FCBNode = FCBNode(0)
+        private var curr : FCBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
