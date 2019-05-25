fee25fa17d833213fd2e356ee92fe9bee8d0369e
Anwesh43 <anweshthecool0@gmail.com>
2019-05-25 17:14:12 +0530

    adding constants and libraries

--- fourcrossboxview/src/main/java/com/anwesh/uiprojects/fourcrossboxview/FourCrossBoxView.kt
+++ fourcrossboxview/src/main/java/com/anwesh/uiprojects/fourcrossboxview/FourCrossBoxView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.fourcrossboxview
 /**
  * Created by anweshmishra on 25/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
